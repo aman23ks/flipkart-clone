5ce4b88a51253488d66db8fe6dba8ac8048043d6
Aman Shrivastava <nks.ns84@gmail.com>
2020-11-22 22:42:58 +0530

    validation request

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router(); //Express router is a class which helps us to create router handlers. By router handler i mean to not just providing routing to our app but also can extend this routing to handle validation.
 const User = require("../../models/user");
 const { signup, signin } = require("../../controller/admin/auth");
+const {
+  isRequestValidated,
+  validateSignupRequest,
+  validateSigninRequest,
+} = require("../../validators/auth");
 
-router.post("/admin/signin", signin);
+router.post("/admin/signin", validateSigninRequest, isRequestValidated, signin);
 
-router.post("/admin/signup", signup);
+router.post("/admin/signup", validateSignupRequest, isRequestValidated, signup);
 
 module.exports = router;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router(); //Express router is a class which helps us to create router handlers. By router handler i mean to not just providing routing to our app but also can extend this routing to handle validation.
 const User = require("../models/user");
 const { signup, signin } = require("../controller/auth");
+const {
+  validateSignupRequest,
+  isRequestValidated,
+  validateSigninRequest,
+} = require("../validators/auth");
 
-router.post("/signin", signin);
+router.post("/signin", validateSigninRequest, isRequestValidated, signin);
 
-router.post("/signup", signup);
+router.post("/signup", validateSignupRequest, isRequestValidated, signup);
 
 // //if a user is logged in  how he can navigate to some protected routes such as profile page or somewhere where he needs to be a logged in user.
 // router.post("/profile", requireSignin, (req, res) => {
