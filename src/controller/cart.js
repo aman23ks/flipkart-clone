12b21afebacccb449813d7f7375a6e2717c8e32f
Aman Shrivastava <nks.ns84@gmail.com>
2020-11-26 23:56:13 +0530

    fix cart api & update category api

--- src/controller/cart.js
+++ src/controller/cart.js
@@ -7,42 +7,32 @@ exports.addItemToCart = (req, res) => {
       // if cart already exists update cart by quantity
       const product = req.body.cartItems.product;
       const item = cart.cartItems.find((c) => c.product == product); //we are finding the same product so c.product == product
-
+      let condition, update;
       if (item) {
-        Cart.findOneAndUpdate(
-          //cartItems is a property
-          { user: req.user._id, "cartItems.product": product },
-          {
-            $set: {
-              cartItems: {
-                ...req.body.cartItems,
-                quantity: item.quantity + req.body.cartItems.quantity,
-              },
+        condition = { user: req.user._id, "cartItems.product": product };
+        update = {
+          $set: {
+            "cartItems.$": {
+              ...req.body.cartItems,
+              quantity: item.quantity + req.body.cartItems.quantity,
             },
-          }
-        ).exec((error, _cart) => {
-          if (error) return res.status(400).json({ error });
-          if (_cart) {
-            res.status(201).json({ cart: _cart });
-          }
-        });
+          },
+        };
       } else {
-        Cart.findOneAndUpdate(
-          { user: req.user._id },
-          {
-            $push: {
-              //"$push" is how we push an element into a subarray
-              cartItems: req.body.cartItems,
-            },
-          }
-        ).exec((error, _cart) => {
-          if (error) return res.status(400).json({ error });
-          if (_cart) {
-            res.status(201).json({ cart: _cart });
-          }
-        });
+        condition = { user: req.user._id };
+        update = {
+          $push: {
+            //"$push" is how we push an element into a subarray
+            cartItems: req.body.cartItems,
+          },
+        };
       }
-
+      Cart.findOneAndUpdate(condition, update).exec((error, _cart) => {
+        if (error) return res.status(400).json({ error });
+        if (_cart) {
+          res.status(201).json({ cart: _cart });
+        }
+      });
       //this is to check if the user already exists, now we have to check if the item already exists so that we can update the quantity.
     } else {
       // if cart does not exist then create a new cart
--- src/index.server.js
+++ src/index.server.js
@@ -3,6 +3,7 @@ const app = express();
 const env = require("dotenv");
 const bodyParser = require("body-parser"); //we can use app.use(express.json()) to parse the data but its better to use a library that is made specifically for this purpose which is body-parser.
 const mongoose = require("mongoose");
+const path = require("path");
 
 //routes
 const authRoutes = require("./routes/auth");
@@ -34,7 +35,7 @@ app.use(express.json()); //since we are using Json data we are using json to par
 //   })
 // );
 // app.use(bodyParser.json());
-
+app.use("/public", express.static(path.join(__dirname, "uploads")));
 //Middlewares : When you are making a request from POSTMAN or from a browser, and at the backend you are handling the request in between making the request and handling the request if you manipulate the data according to your requirement thats a middleware , so we'll write some functions so we can do that.
 app.use("/api", authRoutes); //Every request will be prefixed with 'api' for the userRoutes and the useRoutes will be called which are available in '/routes/user.js'
 app.use("/api", adminRoutes);
